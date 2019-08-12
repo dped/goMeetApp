b3eb2e8136f092669f59299ecdd1e46ba28794e2
dorte pedersen <x@dortepedersen.dk>
2019-08-12 17:43:26 +0200

    Added Models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_interests
+  has_many :interests, through: :user_interests
+
+  has_many :bookings
+  has_many :events, through: :bookings
+  has_many :host_events, class_name: 'Event'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end

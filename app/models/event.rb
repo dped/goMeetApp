16ff1b528bc7f1065db412e229522f02ac7d1770
dorte pedersen <x@dortepedersen.dk>
2019-08-15 16:56:31 +0200

    work in progress

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :description, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  validates :start_time, presence: true
+  validates :price, presence: true
+  validates :end_time, presence: true
+  validates :title, presence: true
+  validates :address, presence: true
 end

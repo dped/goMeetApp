483bb81f64d387a24f21f463b68b59c5dc036695
Viktoria Zubchenko <Zubchenko0508@gmail.com>
2019-08-15 13:30:59 +0200

    seeds are added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_122157) do
+ActiveRecord::Schema.define(version: 2019_08_15_111104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_122157) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -66,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_122157) do
     t.string "last_name"
     t.text "description"
     t.string "location"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
